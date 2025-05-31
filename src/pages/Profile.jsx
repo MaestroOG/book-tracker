b505a87fb59c5732b02a6f2e267d04f05b583d93
MaestroOG <muneeburrehman822@gmail.com>
2025-05-31 13:18:15 +0500

    show name & email on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,8 +2,11 @@ import { useNavigate } from "react-router-dom"
 import { useFirebase } from "../context/Firebase"
 
 const Profile = () => {
+    const { user } = useFirebase()
     const firebase = useFirebase()
     const navigate = useNavigate()
+
+    console.log(user)
 
     const handleLogoutClick = async () => {
         try {
@@ -19,8 +22,8 @@ const Profile = () => {
             <section className="login-card flex flex-col items-center justify-center space-y-6">
                 <img src="https://avatar.iran.liara.run/public/20" alt="avatar" width={120} height={120} loading="eager" />
                 <div className="text-center">
-                    <h1>John Doe</h1>
-                    <p className="text-muted">johndoe@gmail.com</p>
+                    <h1>{user?.email.slice(0, 6)}</h1>
+                    <p className="text-muted">{user?.email}</p>
                 </div>
                 <div className="flex items-center gap-5">
                     <button className="btn-primary" onClick={handleLogoutClick}>Logout</button>
